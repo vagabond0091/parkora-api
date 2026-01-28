5cae6d1795c6aa9c6a3339c319428d7e112c6be0
Jerry Endaya <vagabondendaya@gmail.com>
2026-01-28 19:42:07 +0800

    Change the model attribute to requestBody in the authenticateUser

--- api/src/main/java/com/parkora/api/controller/AuthController.java
+++ api/src/main/java/com/parkora/api/controller/AuthController.java
@@ -11,8 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +25,8 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping(value = "/login", consumes = {"application/json", "multipart/form-data", "application/x-www-form-urlencoded"})
-    public ResponseEntity<ApiResponse<String>> authenticateUser(@Valid @ModelAttribute LoginRequest loginRequest) {
+    @PostMapping(value = "/login", consumes = {"application/json"})
+    public ResponseEntity<ApiResponse<String>> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         try {
             // Authenticate and get JWT token
             String jwt = authService.authenticate(loginRequest);
